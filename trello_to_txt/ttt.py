415d423fbc9f56d0e998c34966c1f469649b4abe
Ben <bkrchr@gmail.com>
2014-01-23 11:37:05 -0500

    Just take the dict as we get it. Slower, but much cleaner.

--- trello_to_txt/ttt.py
+++ trello_to_txt/ttt.py
@@ -1,28 +1,16 @@
 import urllib.request, json
 import trello_to_txt.config
+import operator
 
 def main():
 	bytes = urllib.request.urlopen(trello_to_txt.config.board_url + '?key=' + trello_to_txt.config.dev_key + '&token=' + trello_to_txt.config.token + '&cards=open&lists=open').read()
 	trello_data = json.loads(bytes.decode('utf-8'))
-	output_data = {}
-	trello_list_names = {}
-	trello_list_positions = {}
-	trello_card_positions = {}
 	output_text = ''
 
-	for l in trello_data['lists']:
-		output_data[l['name']] = []
-		trello_list_names[l['id']] = l['name']
-		trello_list_positions[l['name']] = int(l['pos'])
-
-	for c in trello_data['cards']:
-		output_data[trello_list_names[c['idList']]].append(c['name'])
-		trello_card_positions[c['name']] = int(c['pos'])
-
-	for list_name in sorted(output_data.keys(), key=trello_list_positions.get):
-		output_text += '# ' + list_name + '\n\n'
-		for card_name in sorted(output_data[list_name], key=trello_card_positions.get):
-			output_text += '* ' + card_name + '\n'
+	for l in sorted(trello_data['lists'], key=operator.itemgetter('pos')):
+		output_text += '# ' + l['name'] + '\n\n'
+		for card in sorted((card for card in trello_data['cards'] if card['idList'] == l['id']), key=operator.itemgetter('pos')):
+			output_text += '* ' + card['name'] + '\n'
 		output_text += '\n'
 
 	print(output_text)
