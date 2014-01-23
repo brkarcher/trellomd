0e583ea2db10fa282abe8d2f81b9cbe5b664f065
Ben <bkrchr@gmail.com>
2014-01-23 11:24:11 -0500

    Sort as they'd look on trello

--- trello_to_txt/ttt.py
+++ trello_to_txt/ttt.py
@@ -6,18 +6,22 @@ def main():
 	trello_data = json.loads(bytes.decode('utf-8'))
 	output_data = {}
 	trello_list_names = {}
+	trello_list_positions = {}
+	trello_card_positions = {}
 	output_text = ''
 
 	for l in trello_data['lists']:
 		output_data[l['name']] = []
 		trello_list_names[l['id']] = l['name']
+		trello_list_positions[l['name']] = int(l['pos'])
 
 	for c in trello_data['cards']:
 		output_data[trello_list_names[c['idList']]].append(c['name'])
+		trello_card_positions[c['name']] = int(c['pos'])
 
-	for list_name in output_data:
+	for list_name in sorted(output_data.keys(), key=trello_list_positions.get):
 		output_text += '# ' + list_name + '\n\n'
-		for card_name in output_data[list_name]:
+		for card_name in sorted(output_data[list_name], key=trello_card_positions.get):
 			output_text += '* ' + card_name + '\n'
 		output_text += '\n'
 
