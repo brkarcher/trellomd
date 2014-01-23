38e005a331383b65808f85922e3c6ecb2023fe8d
Ben <bkrchr@gmail.com>
2014-01-23 11:46:22 -0500

    omit parens for unlabeled cards.

--- trello_to_txt/ttt.py
+++ trello_to_txt/ttt.py
@@ -10,7 +10,10 @@ def main():
 	for l in sorted(trello_data['lists'], key=operator.itemgetter('pos')):
 		output_text += '# ' + l['name'] + '\n\n'
 		for card in sorted((card for card in trello_data['cards'] if card['idList'] == l['id']), key=operator.itemgetter('pos')):
-			output_text += '* {0} ({1})\n'.format(card['name'], ', '.join((label['name'] for label in card['labels'])))
+			if not card['labels']:
+				output_text += '* {0}\n'.format(card['name'])
+			else:
+				output_text += '* {0} ({1})\n'.format(card['name'], ', '.join((label['name'] for label in card['labels'])))
 		output_text += '\n'
 
 	print(output_text)
