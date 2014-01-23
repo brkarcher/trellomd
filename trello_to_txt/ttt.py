4da92e69621f3183362ed92e419d60ae01bb1f53
Ben <bkrchr@gmail.com>
2014-01-23 11:42:03 -0500

    Label support.

--- trello_to_txt/ttt.py
+++ trello_to_txt/ttt.py
@@ -10,7 +10,7 @@ def main():
 	for l in sorted(trello_data['lists'], key=operator.itemgetter('pos')):
 		output_text += '# ' + l['name'] + '\n\n'
 		for card in sorted((card for card in trello_data['cards'] if card['idList'] == l['id']), key=operator.itemgetter('pos')):
-			output_text += '* ' + card['name'] + '\n'
+			output_text += '* {0} ({1})\n'.format(card['name'], ', '.join((label['name'] for label in card['labels'])))
 		output_text += '\n'
 
 	print(output_text)
